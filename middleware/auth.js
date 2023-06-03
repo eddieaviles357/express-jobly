d0ad2698860464a5c58a39b934851aad6bb1784a
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-02 21:50:17 -0700

    updated statement to check agains actual value of false

--- middleware/auth.js
+++ middleware/auth.js
@@ -48,7 +48,7 @@ function ensureLoggedIn(req, res, next) {
  */
 function ensureAdmin(req, res, next) {
   try {
-    if (!res.locals.user.isAdmin) throw new UnauthorizedError();
+    if (res.locals.user.isAdmin === false) throw new UnauthorizedError();
     return next();
   } catch (err) {
     return next(err);
