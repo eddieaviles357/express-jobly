50f91302954920e97d78f9ee4c0a785e765491c1
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-06 19:51:44 -0700

    imported jobsNewSchema to check against incoming body from users, implemented validations of req.body

--- routes/jobs.js
+++ routes/jobs.js
@@ -9,6 +9,8 @@ const { BadRequestError } = require("../expressError");
 const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
 const Job = require("../models/job");
 
+const jobsNewSchema = require("../schemas/jobsNew.json");
+
 const router = new express.Router();
 
 
@@ -23,6 +25,13 @@ const router = new express.Router();
 
 router.post("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
     try {
+      console.log('jobs',req.body);
+      const validator = jsonschema.validate(req.body, jobsNewSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+      
       const job = await Job.create(req.body);
       return res.status(201).json({ job });
     } catch (err) {
