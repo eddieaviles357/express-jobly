c88ebfff0a0ba68c3aa05b21a26591e359ad9551
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-06 21:07:48 -0700

    update job is not active

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,6 +10,7 @@ const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
 const Job = require("../models/job");
 
 const jobsNewSchema = require("../schemas/jobsNew.json");
+const jobsUpdateSchema = require("../schemas/jobsUpdate.json");
 
 const router = new express.Router();
 
@@ -66,6 +67,32 @@ router.post("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
 router.get("/:id", async function (req, res, next) {
   try {
     const job = await Job.get(req.params.id);
+    return res.json({ job });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /[id] { fld1, fld2, ... } => { job }
+ *
+ * Patches job data.
+ *
+ * fields can be: { title, salary, equity }
+ *
+ * Returns { id, title, salary, equity, company_handle }
+ *
+ * Authorization required: login, Admin
+ */
+
+router.patch("/:id", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, jobsUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const job = await Job.update(req.params.id, req.body);
     return res.json({ job });
   } catch (err) {
     return next(err);
