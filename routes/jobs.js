9035c6f55b8377231c939b88fad12ff664d58629
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-06 19:20:31 -0700

    post route added for creating a job

---
+++ routes/jobs.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/** Routes for jobs. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const Job = require("../models/job");
+
+const router = new express.Router();
+
+
+/** POST / { job } =>  { job }
+ *
+ * job should be { title, salary, equity, company_handle }
+ *
+ * Returns { id, title, salary, equity, company_handle }
+ *
+ * Authorization required: login
+ */
+
+router.post("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
+    try {
+      const job = await Job.create(req.body);
+      return res.status(201).json({ job });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+module.exports = router;
