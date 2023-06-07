91eaccca25b0392eb5a620467e362964bbc0c143
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-06 19:24:37 -0700

    post job test added

--- routes/jobs.tests.js
+++ routes/jobs.tests.js
@@ -28,7 +28,22 @@ describe("POST /jobs", function () {
         equity: "0", // When using NUMERIC with postgresql it gets treated as a string
         company_handle: "new",
       };
-  
+
+    test("ok for Admin users", async function () {
+        const resp = await request(app)
+            .post("/jobs")
+            .send(newJob)
+            .set("authorization", `Bearer ${u1Token}`);
+        console.log(resp);
+        expect(resp.statusCode).toEqual(201);
+        // const { id, title, salary, equity, company_handle } = resp;
+
+        // expect(id).toEqual(expect.any(Number));
+        
+        // expect({title, salary, equity, company_handle}).toEqual({
+        //     job: newJob,
+        // });
+    });
 });
   
 /************************************** GET /jobs */
